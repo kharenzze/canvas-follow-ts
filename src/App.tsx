b1a7beac114014a90c7de452605a682c60b6517d
Paolo <str237@gmail.com>
2021-07-03 19:53:48 +0200

    Format code and handle click

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Canvas } from "react-three-fiber";
-import { Circle } from "./components/Circle";
-import { Background } from "./components/Background";
-import { ZERO3 } from "./constants";
-import './App.css';
+import React, { useCallback, useState } from 'react'
+import { Canvas } from "react-three-fiber"
+import { Circle } from "./components/Circle"
+import { Background } from "./components/Background"
+import { ZERO3 } from "./constants"
+import './App.css'
 
 function App() {
+  const [target, setTarget] = useState(() => ZERO3)
+  const onClick = useCallback(evt => {
+    setTarget(evt.point)
+  }, [])
   return (
     <div className="App">
-        <Canvas>
-            <ambientLight/>
-            <pointLight position={[10, 10, 10]}/>
-            <Background/>
-            <Circle position={ZERO3}
-                    scale={0.2}
-            />
-        </Canvas>
+      <Canvas>
+        <ambientLight/>
+        <pointLight position={[10, 10, 10]}/>
+        <Background onClick={onClick}/>
+        <Circle position={ZERO3}
+                scale={0.2}
+        />
+      </Canvas>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
